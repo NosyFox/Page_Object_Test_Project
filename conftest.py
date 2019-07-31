76c6c3b0dacb03ef665a9c05869e92412f9cb60a
Alex <nosyfox@stepik.test>
2019-07-31 10:25:34 +0300

    Подготовка тестового окружения

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default="Chrome")
+    parser.addoption('--language', action='store', default="en")
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+	browser = request.config.getoption("browser")
+	user_language = request.config.getoption("language")
+	if (browser == "Chrome"):
+		options = Options()
+		options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+		browser = webdriver.Chrome(options=options)
+	elif (browser == "Firefox"):
+		fp = webdriver.FirefoxProfile()
+		fp.set_preference("intl.accept_languages", user_language)
+		browser = webdriver.Firefox(firefox_profile=fp)
+	yield browser
+	browser.quit()
